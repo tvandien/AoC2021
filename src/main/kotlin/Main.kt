53249e20c2021701b25fd4d1596441705b70ca20
Tijmen van Dien <git@ndien.nl>
2021-12-06 09:52:57 +0100

    Day 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ import aoc.*
 import java.io.File
 
 fun main() {
-    day5a()
-    day5b()
+    day6a()
+    day6b()
 }
 
 fun readFile(file: String): List<String> = File("src/main/resources/$file").readLines()
---
+++ src/main/kotlin/aoc/day6.kt
@@ -0,0 +1,35 @@
+package aoc
+
+import readFile
+
+fun day6a(days: Int = 80) {
+    val lines = readFile("day6_input.txt")
+
+    val initialFish = lines.first().split(",").map { it.toInt() }
+    var fishByAge = MutableList<Long>(9) { 0 }
+
+    for (fish in initialFish) {
+        fishByAge[fish]++
+    }
+
+    for (i in 0 until days) {
+        fishByAge = simulateDay(fishByAge)
+    }
+
+    val count = fishByAge.sum()
+
+    println("After $days days: $count")
+}
+
+fun simulateDay(today: MutableList<Long>): MutableList<Long> {
+    return MutableList(9) { index ->
+        when (index) {
+            6 -> today[7] + today[0]
+            else -> today[(index + 1) % 9]
+        }
+    }
+}
+
+fun day6b() {
+    day6a(256)
+}
