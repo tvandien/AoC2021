b365a20bf0b6636ddc18add193607b7c40f67cc0
Tijmen van Dien <git@ndien.nl>
2021-12-07 09:04:16 +0100

    Day 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ import aoc.*
 import java.io.File
 
 fun main() {
-    day6a()
-    day6b()
+    day7a()
+    day7b()
 }
 
 fun readFile(file: String): List<String> = File("src/main/resources/$file").readLines()
---
+++ src/main/kotlin/aoc/day7.kt
@@ -0,0 +1,40 @@
+package aoc
+
+import readFile
+import kotlin.math.abs
+
+fun day7a() {
+    val lines = readFile("day7_input.txt")
+    val numbers = lines.first().split(",").map { it.toInt() }.sorted()
+
+    val median = numbers[numbers.size / 2]
+    val score = numbers.sumOf { abs(it - median) }
+
+    println("cheapest at position $median. Score: $score")
+}
+
+fun day7b() {
+    val lines = readFile("day7_input.txt")
+    val numbers = lines.first().split(",").map { it.toInt() }.sorted()
+
+    var minimalScore = Int.MAX_VALUE
+    var minimalPosition = -1
+    for (i in 0..numbers.last()) {
+        val score = numbers.sumOf { crabScore(abs(it - i)) }
+        if (score < minimalScore) {
+            minimalScore = score
+            minimalPosition = i
+        }
+        println("$i\t$score")
+    }
+
+    println("cheapest at position $minimalPosition. Score: $minimalScore")
+}
+
+fun crabScore(distance: Int): Int {
+    return if (distance % 2 == 0) {
+        (distance + 1) * (distance / 2)
+    } else {
+        (distance + 1) * (distance / 2) + (distance / 2) + 1
+    }
+}
